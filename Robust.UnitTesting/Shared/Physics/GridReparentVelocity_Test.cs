ff0c5641d7ee18a1ca3be381eae5d7c7289da372
Whatstone <whatston3@gmail.com>
2025-04-03 22:38:36 -0400

    Cleanup

--- Robust.UnitTesting/Shared/Physics/GridReparentVelocity_Test.cs
+++ Robust.UnitTesting/Shared/Physics/GridReparentVelocity_Test.cs
@@ -29,172 +29,173 @@ public sealed class GridReparentVelocity_Test : RobustIntegrationTest
         hard: false
 ";
 
+    private ISimulation _sim = default!;
+    private IEntitySystemManager _systems = default!;
+    private IEntityManager _entManager = default!;
+    private IMapManager _mapManager = default!;
+    private SharedPhysicsSystem _physSystem = default!;
+    private SharedMapSystem _mapSystem = default!;
+
+    // Test objects.
+    private EntityUid _mapUid = default!;
+    private EntityUid _gridUid = default!;
+    private EntityUid _objUid = default!;
+
+    [OneTimeSetUp]
+    public void FixtureSetup()
+    {
+        _sim = RobustServerSimulation.NewSimulation()
+            .RegisterPrototypes(protoMan => protoMan.LoadString(Prototypes))
+            .InitializeInstance();
+
+        _systems = _sim.Resolve<IEntitySystemManager>();
+        _entManager = _sim.Resolve<IEntityManager>();
+        _mapManager = _sim.Resolve<IMapManager>();
+        _physSystem = _systems.GetEntitySystem<SharedPhysicsSystem>();
+        _mapSystem = _systems.GetEntitySystem<SharedMapSystem>();
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        _mapUid = _mapSystem.CreateMap(out var mapId);
+        _gridUid = SetupTestGrid(mapId);
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        _entManager.DeleteEntity(_gridUid);
+        _gridUid = default!;
+        _entManager.DeleteEntity(_objUid);
+        _objUid = default!;
+    }
+
     // Moves an object off of a moving grid, checks for conservation of linear velocity.
     [Test]
     public async Task TestLinearVelocityOnlyMoveOffGrid()
     {
-        var sim = RobustServerSimulation.NewSimulation();
-        var server = sim.InitializeInstance();
-
-        var systems = server.Resolve<IEntitySystemManager>();
-        var entManager = server.Resolve<IEntityManager>();
-        var physSystem = systems.GetEntitySystem<SharedPhysicsSystem>();
-        var mapSystem = entManager.EntitySysManager.GetEntitySystem<SharedMapSystem>();
-
         // Spawn our test object in the middle of the grid, ensure it has no damping.
-        EntityUid map = mapSystem.CreateMap(out var mapId);
-        EntityUid grid = SetupTestGrid(mapId, server, systems, entManager, mapSystem);
-        EntityUid obj = SetupTestObject(new EntityCoordinates(grid, 0.5f, 0.5f), physSystem, entManager);
+        EntityUid obj = SetupTestObject(new EntityCoordinates(grid, 0.5f, 0.5f));
 
         // Our object should start on the grid.
-        Assert.That(entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(grid));
+        Assert.That(_entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(grid));
         
         // Set the velocity of the grid and our object.
-        Assert.That(physSystem.SetLinearVelocity(obj, new Vector2(3.5f, 4.75f)), Is.True);
-        Assert.That(physSystem.SetLinearVelocity(grid, new Vector2(1.0f, 2.0f)), Is.True);
+        Assert.That(_physSystem.SetLinearVelocity(obj, new Vector2(3.5f, 4.75f)), Is.True);
+        Assert.That(_physSystem.SetLinearVelocity(grid, new Vector2(1.0f, 2.0f)), Is.True);
 
         // Wait a second to clear the grid
-        physSystem.Update(1.0f);
+        _physSystem.Update(1.0f);
 
         // The object should be parented to the map and maintain its map velocity, the grid should be unchanged.
-        Assert.That(entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(map));
-        Assert.That(entManager.GetComponent<PhysicsComponent>(obj).LinearVelocity, Is.EqualTo(new Vector2(4.5f, 6.75f)));
-        Assert.That(entManager.GetComponent<PhysicsComponent>(grid).LinearVelocity, Is.EqualTo(new Vector2(1.0f, 2.0f)));
+        Assert.That(_entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(map));
+        Assert.That(_entManager.GetComponent<PhysicsComponent>(obj).LinearVelocity, Is.EqualTo(new Vector2(4.5f, 6.75f)));
+        Assert.That(_entManager.GetComponent<PhysicsComponent>(grid).LinearVelocity, Is.EqualTo(new Vector2(1.0f, 2.0f)));
     }
 
-    // Moves an object onto a moving grid, checks for conservation of linear velocity.
     [Test]
+    // Moves an object onto a moving grid, checks for conservation of linear velocity.
     public async Task TestLinearVelocityOnlyMoveOntoGrid()
     {
-        var sim = RobustServerSimulation.NewSimulation();
-        var server = sim.InitializeInstance();
-
-        var systems = server.Resolve<IEntitySystemManager>();
-        var entManager = server.Resolve<IEntityManager>();
-        var physSystem = systems.GetEntitySystem<SharedPhysicsSystem>();
-        var mapSystem = entManager.EntitySysManager.GetEntitySystem<SharedMapSystem>();
-
         // Spawn our test object 1 m off of the middle of the grid in both directions, ensure it has no damping.
-        EntityUid map = mapSystem.CreateMap(out var mapId);
-        EntityUid grid = SetupTestGrid(mapId, server, systems, entManager, mapSystem);
-        EntityUid obj = SetupTestObject(new EntityCoordinates(map, 1.5f, 1.5f), physSystem, entManager);
+        EntityUid obj = SetupTestObject(new EntityCoordinates(map, 1.5f, 1.5f));
 
         // Assert that we start off the grid.
-        Assert.That(entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(map));
+        Assert.That(_entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(map));
         
         // Set the velocity of the grid and our object.
-        Assert.That(physSystem.SetLinearVelocity(obj, new Vector2(-2.0f, -3.0f)), Is.True);
-        Assert.That(physSystem.SetLinearVelocity(grid, new Vector2(-1.0f, -2.0f)), Is.True);
+        Assert.That(_physSystem.SetLinearVelocity(obj, new Vector2(-2.0f, -3.0f)), Is.True);
+        Assert.That(_physSystem.SetLinearVelocity(grid, new Vector2(-1.0f, -2.0f)), Is.True);
 
         // Wait a second to move onto the middle of the grid
-        physSystem.Update(1.0f);
+        _physSystem.Update(1.0f);
 
         // The object should be parented to the grid and maintain its map velocity (slowing down), the grid should be unchanged.
-        Assert.That(entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(grid));
-        Assert.That(entManager.GetComponent<PhysicsComponent>(obj).LinearVelocity, Is.EqualTo(new Vector2(-1.0f, -1.0f)));
-        Assert.That(entManager.GetComponent<PhysicsComponent>(grid).LinearVelocity, Is.EqualTo(new Vector2(-1.0f, -2.0f)));
+        Assert.That(_entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(grid));
+        Assert.That(_entManager.GetComponent<PhysicsComponent>(obj).LinearVelocity, Is.EqualTo(new Vector2(-1.0f, -1.0f)));
+        Assert.That(_entManager.GetComponent<PhysicsComponent>(grid).LinearVelocity, Is.EqualTo(new Vector2(-1.0f, -2.0f)));
     }
 
-    // Moves a rotating object off of a rotating grid, checks for conservation of angular velocity.
     [Test]
+    // Moves a rotating object off of a rotating grid, checks for conservation of angular velocity.
     public async Task TestLinearAndAngularVelocityMoveOffGrid()
     {
-        var sim = RobustServerSimulation.NewSimulation();
-        var server = sim.InitializeInstance();
-
-        var systems = server.Resolve<IEntitySystemManager>();
-        var entManager = server.Resolve<IEntityManager>();
-        var physSystem = systems.GetEntitySystem<SharedPhysicsSystem>();
-        var mapSystem = systems.GetEntitySystem<SharedMapSystem>();
-
         // Spawn our test object in the middle of the grid, ensure it has no damping.
-        EntityUid map = mapSystem.CreateMap(out var mapId);
-        EntityUid grid = SetupTestGrid(mapId, server, systems, entManager, mapSystem);
-        EntityUid obj = SetupTestObject(new EntityCoordinates(grid, 0.5f, 0.5f), physSystem, entManager);
+        EntityUid obj = SetupTestObject(new EntityCoordinates(grid, 0.5f, 0.5f));
 
         // Our object should start on the grid.
-        Assert.That(entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(grid));
+        Assert.That(_entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(grid));
         
         // Set the velocity of the grid and our object.
-        Assert.That(physSystem.SetLinearVelocity(obj, new Vector2(3.5f, 4.75f)), Is.True);
-        Assert.That(physSystem.SetAngularVelocity(obj, 1.0f), Is.True);
-        Assert.That(physSystem.SetLinearVelocity(grid, new Vector2(1.0f, 2.0f)), Is.True);
-        Assert.That(physSystem.SetAngularVelocity(grid, 2.0f), Is.True);
+        Assert.That(_physSystem.SetLinearVelocity(obj, new Vector2(3.5f, 4.75f)), Is.True);
+        Assert.That(_physSystem.SetAngularVelocity(obj, 1.0f), Is.True);
+        Assert.That(_physSystem.SetLinearVelocity(grid, new Vector2(1.0f, 2.0f)), Is.True);
+        Assert.That(_physSystem.SetAngularVelocity(grid, 2.0f), Is.True);
 
         // Wait a second to clear the grid
-        physSystem.Update(1.0f);
+        _physSystem.Update(1.0f);
 
         // The object should be parented to the map and maintain its map velocity, the grid should be unchanged.
-        Assert.That(entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(map));
+        Assert.That(_entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(map));
         // Not checking object's linear velocity in this case, non-zero contribution from grid angular velocity.
-        Assert.That(entManager.GetComponent<PhysicsComponent>(obj).AngularVelocity, Is.EqualTo(3.0f));
-        var gridPhys = entManager.GetComponent<PhysicsComponent>(grid);
+        Assert.That(_entManager.GetComponent<PhysicsComponent>(obj).AngularVelocity, Is.EqualTo(3.0f));
+        var gridPhys = _entManager.GetComponent<PhysicsComponent>(grid);
         Assert.That(gridPhys.LinearVelocity, Is.EqualTo(new Vector2(1.0f, 2.0f)));
         Assert.That(gridPhys.AngularVelocity, Is.EqualTo(2.0f));
     }
 
-    // Moves a rotating object onto a rotating grid, checks for conservation of angular velocity.
     [Test]
+    // Moves a rotating object onto a rotating grid, checks for conservation of angular velocity.
     public async Task TestLinearAndAngularVelocityMoveOntoGrid()
     {
-        var sim = RobustServerSimulation.NewSimulation();
-        var server = sim.InitializeInstance();
-
-        var systems = server.Resolve<IEntitySystemManager>();
-        var entManager = server.Resolve<IEntityManager>();
-        var physSystem = systems.GetEntitySystem<SharedPhysicsSystem>();
-        var mapSystem = systems.GetEntitySystem<SharedMapSystem>();
-
         // Spawn our test object in the middle of the grid, ensure it has no damping.
-        EntityUid map = mapSystem.CreateMap(out var mapId);
-        EntityUid grid = SetupTestGrid(mapId, server, systems, entManager, mapSystem);
-        EntityUid obj = SetupTestObject(new EntityCoordinates(map, 1.5f, 1.5f), physSystem, entManager);
+        EntityUid obj = SetupTestObject(new EntityCoordinates(map, 1.5f, 1.5f));
 
         // Assert that we start off the grid.
-        Assert.That(entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(map));
+        Assert.That(_entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(map));
         
         // Set the velocity of the grid and our object.
-        Assert.That(physSystem.SetLinearVelocity(obj, new Vector2(-2.0f, -3.0f)), Is.True);
-        Assert.That(physSystem.SetAngularVelocity(obj, 1.0f), Is.True);
-        Assert.That(physSystem.SetLinearVelocity(grid, new Vector2(-1.0f, -2.0f)), Is.True);
-        Assert.That(physSystem.SetAngularVelocity(grid, 2.0f), Is.True);
+        Assert.That(_physSystem.SetLinearVelocity(obj, new Vector2(-2.0f, -3.0f)), Is.True);
+        Assert.That(_physSystem.SetAngularVelocity(obj, 1.0f), Is.True);
+        Assert.That(_physSystem.SetLinearVelocity(grid, new Vector2(-1.0f, -2.0f)), Is.True);
+        Assert.That(_physSystem.SetAngularVelocity(grid, 2.0f), Is.True);
 
         // Wait a second to move onto the middle of the grid
-        physSystem.Update(1.0f);
+        _physSystem.Update(1.0f);
 
         // The object should be parented to the grid and maintain its map velocity (slowing down), the grid should be unchanged.
-        Assert.That(entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(grid));
+        Assert.That(_entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(grid));
         // Not checking object's linear velocity in this case, non-zero contribution from grid angular velocity.
-        Assert.That(entManager.GetComponent<PhysicsComponent>(obj).AngularVelocity, Is.EqualTo(-1.0f));
-        var gridPhys = entManager.GetComponent<PhysicsComponent>(grid);
+        Assert.That(_entManager.GetComponent<PhysicsComponent>(obj).AngularVelocity, Is.EqualTo(-1.0f));
+        var gridPhys = _entManager.GetComponent<PhysicsComponent>(grid);
         Assert.That(gridPhys.LinearVelocity, Is.EqualTo(new Vector2(-1.0f, -2.0f)));
         Assert.That(gridPhys.AngularVelocity, Is.EqualTo(2.0f));
     }
 
     // Spawn a 1x1 grid centered at (0.5, 0.5), ensure it's movable and its velocity has no damping.
-    public EntityUid SetupTestGrid(MapId map, ISimulation server, IEntitySystemManager systems, IEntityManager entManager, SharedMapSystem mapSystem)
+    public EntityUid SetupTestGrid(MapId map)
     {
-        var physSystem = systems.GetEntitySystem<SharedPhysicsSystem>();
-        var mapManager = server.Resolve<IMapManager>();
+        var physSystem = _systems.GetEntitySystem<SharedPhysicsSystem>();
 
-        var gridEnt = mapManager.CreateGridEntity(map);
+        var gridEnt = _mapManager.CreateGridEntity(map);
         physSystem.SetCanCollide(gridEnt, true);
         physSystem.SetBodyType(gridEnt, BodyType.Dynamic);
-        var gridPhys = entManager.GetComponent<PhysicsComponent>(gridEnt);
+        var gridPhys = _entManager.GetComponent<PhysicsComponent>(gridEnt);
         physSystem.SetLinearDamping(gridEnt, gridPhys, 0.0f);
         physSystem.SetAngularDamping(gridEnt, gridPhys, 0.0f);
 
-        mapSystem.SetTile(gridEnt, Vector2i.Zero, new Tile(1));
+        _mapSystem.SetTile(gridEnt, Vector2i.Zero, new Tile(1));
         return gridEnt.Owner;
     }
 
     // Spawn a test object at the given position, ensure its velocity has no damping.
-    public EntityUid SetupTestObject(EntityCoordinates coords, SharedPhysicsSystem physSystem, IEntityManager entManager)
+    public EntityUid SetupTestObject(EntityCoordinates coords)
     {
-        var obj = entManager.SpawnEntity("ReparentTestObject", coords);
-        physSystem.SetCanCollide(obj, true);
-        physSystem.SetLinearDamping(obj, entManager.GetComponent<PhysicsComponent>(obj), 0.0f);
-        physSystem.SetAngularDamping(obj, entManager.GetComponent<PhysicsComponent>(obj), 0.0f);
+        var obj = _entManager.SpawnEntity("ReparentTestObject", coords);
+        _physSystem.SetCanCollide(obj, true);
+        _physSystem.SetLinearDamping(obj, _entManager.GetComponent<PhysicsComponent>(obj), 0.0f);
+        _physSystem.SetAngularDamping(obj, _entManager.GetComponent<PhysicsComponent>(obj), 0.0f);
 
         return obj;
     }
