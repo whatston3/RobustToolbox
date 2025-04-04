8ac79a09eefa236ed84a4cf4f1888f3f6e79deb9
Whatstone <whatston3@gmail.com>
2025-04-03 20:22:03 -0400

    Remove object velocity test from rotating grid

--- Robust.UnitTesting/Shared/Physics/GridReparentVelocity_Test.cs
+++ Robust.UnitTesting/Shared/Physics/GridReparentVelocity_Test.cs
@@ -204,9 +204,8 @@ public sealed class GridReparentVelocity_Test : RobustIntegrationTest
 
             // The object should be parented to the map and maintain its map velocity, the grid should be unchanged.
             Assert.That(entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(map));
-            var objPhys = entManager.GetComponent<PhysicsComponent>(obj);
-            Assert.That(objPhys.LinearVelocity, Is.EqualTo(new Vector2(4.5f, 6.75f)));
-            Assert.That(objPhys.AngularVelocity, Is.EqualTo(3.0f));
+            // Not checking object's linear velocity in this case, non-zero contribution from grid angular velocity.
+            Assert.That(entManager.GetComponent<PhysicsComponent>(obj).AngularVelocity, Is.EqualTo(3.0f));
             var gridPhys = entManager.GetComponent<PhysicsComponent>(grid);
             Assert.That(gridPhys.LinearVelocity, Is.EqualTo(new Vector2(1.0f, 2.0f)));
             Assert.That(gridPhys.AngularVelocity, Is.EqualTo(2.0f));
@@ -271,7 +270,7 @@ public sealed class GridReparentVelocity_Test : RobustIntegrationTest
 
             // The object should be parented to the grid and maintain its map velocity (slowing down), the grid should be unchanged.
             Assert.That(entManager.GetComponent<TransformComponent>(obj).ParentUid, Is.EqualTo(grid));
-            // Not checking object's linear velocity in this case, non-zero angular velocity contribution from moving onto the grid.
+            // Not checking object's linear velocity in this case, non-zero contribution from grid angular velocity.
             Assert.That(entManager.GetComponent<PhysicsComponent>(obj).AngularVelocity, Is.EqualTo(-1.0f));
             var gridPhys = entManager.GetComponent<PhysicsComponent>(grid);
             Assert.That(gridPhys.LinearVelocity, Is.EqualTo(new Vector2(-1.0f, -2.0f)));
